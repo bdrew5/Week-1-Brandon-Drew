4b695a69e69c0c29c012c54dd88b7939e5385dd4
Brandon Drew <drewb@wlhs.wlwv.k12.or.us>
2020-04-15 11:40:54 -0700

    fixed files

--- src/ArrayFun.java
+++ src/ArrayFun.java
@@ -17,10 +17,10 @@ public class ArrayFun {
     }
 
     public void mixUp(){
-        mixedNums[0] = nums[0];
-        mixedNums[mixedNums.length-1] = nums[nums.length-1];
+        mixedNums[0] = nums[nums.length-1];
+        mixedNums[mixedNums.length-1] = nums[0];
         for(int i =1; i < nums.length-1; i++){
-            if(nums[i]%nums[i-1] ==0){
+            if(nums[i]%nums[i-1] ==0 ){
                 mixedNums[i] = (nums[i]/nums[i-1]) * nums[i+1];
             }
             else if(nums[i] > nums[i+1]){
@@ -33,12 +33,22 @@ public class ArrayFun {
     }
 
     public int greatestDiff(){
-        int maxNums = 0;
-        int maxMixed = 0;
+        int max = mixedNums[0];
+        int min = mixedNums[0];
         for(int i = 0; i < nums.length; i++){
-            maxMixed += mixedNums[i];
-            maxNums += nums[i];
+            if(mixedNums[i] > max) {
+                max = mixedNums[i];
+            }
+            else if(nums[i]> max) {
+                max = nums[i];
+            }
+            if(mixedNums[i] < min) {
+                min = mixedNums[i];
+            }
+            else if(nums[i]< min) {
+                min = nums[i];
+            }
         }
-        return Math.abs(maxNums - maxMixed);
+        return max-min;
     }
 }
--- src/ArrayFunTester.java
+++ src/ArrayFunTester.java
@@ -9,7 +9,7 @@ public class ArrayFunTester {
         System.out.println("Expected Nums: [5, 10, 3, 9, 15, 4]");
         System.out.println("Actual Nums:" + Arrays.toString(af.getNums()));
 
-        System.out.println("Expected mixedNums: [5, 6, 3, 45, 3, 4]");
+        System.out.println("Expected mixedNums: [4, 6, 3, 45, 3, 5]");
         System.out.println("Actual mixedNums:" + Arrays.toString(af.getMixedNums()));
 
         System.out.println("Expected greatestDiff: 42");
